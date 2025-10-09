ad5e3e4d036a3bd2b4c2312540169bb09db85192
Keshav Worathur <keshavworathur@gmail.com>
2025-10-09 17:14:27 -0400

    feat: implement dynamic data fetching from pokemon api

---
+++ exer5/pokedex/src/components/PokeInfoViewer.tsx
@@ -0,0 +1,45 @@
+export type PokeMove = {
+    move: { name: string }
+}
+
+export type PokeType = {
+    type: { name: string }
+}
+
+export type PokeStat = {
+    stat: { name: string }
+    base_stat?: number
+}
+
+type PokeInfoViewerProps = {
+    title: 'Moves' | 'Info'
+    values: Array<PokeMove | PokeStat>
+}
+
+const PokeInfoViewer = ({ title, values }: PokeInfoViewerProps) => {
+    return (
+        <>
+            <div>
+                <h3 className="mt-2 font-bold">{title}</h3>
+                <div className="mt-2 min-w-64 min-h-72 h-3/4 w-full bg-[#D3D3D3]">
+                    {title === 'Moves'
+                        ? values.map((v, i) => {
+                              return (
+                                  <div key={i}>{(v as PokeMove).move.name}</div>
+                              )
+                          })
+                        : values.map((v, i) => {
+                              return (
+                                  <div key={i}>
+                                      {(v as PokeStat).stat.name}{' '}
+                                      {(v as PokeStat).base_stat}
+                                  </div>
+                              )
+                          })}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default PokeInfoViewer
